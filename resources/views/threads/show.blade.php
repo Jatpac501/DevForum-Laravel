5ad3195eccc12e4310e65d165cb402341c971c1f
Jatpac501 <jatpac501@gmail.com>
2024-12-19 22:57:56 +0400

    фикс кнопки для самоблокировки

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -81,7 +81,7 @@
                                         <form method="POST" action="{{ route('profile.ban', $thread->user) }}"
                                             class="mt-4">
                                             @csrf
-                                            @if ($thread->user->role === 'blocked')
+                                            @if ($thread->user->role === 'blocked' && Auth::id() !== $thread->user->id)
                                                 <button type="submit"
                                                     class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-red-800 border border-transparent rounded-md hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
                                                     Разблокировать
